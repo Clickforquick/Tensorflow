3fde4b23b13c9308d4adf972ac2a25c807a2ebf5
Madhavraob <madhavrao039@gmail.com>
2018-06-21 23:24:47 +0530

    trying to predict

--- use-keras-transfer_learn.py
+++ use-keras-transfer_learn.py
@@ -12,6 +12,29 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 new_model = load_model('inceptionv3-transfer-learning.h5')
 
-new_model.summary()
-new_model.get_weights()
-new_model.optimizer
+# new_model.summary()
+# new_model.get_weights()
+# new_model.optimizer
+
+
+def create_img_generator():
+  return  ImageDataGenerator(
+      preprocessing_function=preprocess_input
+  )
+
+
+# Main Code
+Image_width, Image_height = 299, 299 
+test_dir = './test-images'
+# Batch_Size = 32
+test_batches = ImageDataGenerator().flow_from_directory(test_dir,target_size=(299,299),classes=['daisy','dandelion'],batch_size=10)
+
+predictions = new_model.predict_generator(test_batches,steps=1,verbose=0)
+predictions
+#   Connect the image generator to a folder contains the source images the image generator alters.  
+#   Training image generator
+# train_generator = train_image_gen.flow_from_directory(
+#   train_dir,
+#   target_size=(Image_width, Image_height),
+#   batch_size=batch_size
+# )
